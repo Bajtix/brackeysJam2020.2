486f03588eba5a610ba661ac9bdc6b6ccad6a9e7
Bajtix <Bajtix@users.noreply.github.com>
2020-08-01 13:42:02 +0200

    INITIAL

---
+++ Assets/LegAnimator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegAnimator : MonoBehaviour
+{
+    public Transform legStart;
+    public Transform legTransform;
+
+    private Vector3 lastPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
