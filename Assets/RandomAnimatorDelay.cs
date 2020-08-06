f242f854839a1cfbde4eb4227f302102ba7b771e
Bajtix <Bajtix@users.noreply.github.com>
2020-08-06 23:33:12 +0200

    intro creation progress

---
+++ Assets/RandomAnimatorDelay.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAnimatorDelay : MonoBehaviour
+{
+    public Vector2 randomBetween;
+    private Animator animator;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        animator.enabled = false;
+        LeanTween.delayedCall(Random.Range(randomBetween.x, randomBetween.y),()=>{
+            animator.enabled = true;
+        });
+    }
+}
