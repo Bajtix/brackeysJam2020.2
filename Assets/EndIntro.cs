b15397817cdc127fa71e5c1eaded11854e6b8219
Bajtix <Bajtix@users.noreply.github.com>
2020-08-07 15:03:10 +0200

    intro scene

---
+++ Assets/EndIntro.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class EndIntro : MonoBehaviour
+{
+    public VideoPlayer p1, p2;
+    public GameObject panel;
+    public int nextScene = 2;
+    private void Start()
+    {
+        panel.SetActive(true);
+        p1.Play();
+        p2.Play();
+        AsyncOperation o = SceneManager.LoadSceneAsync(nextScene, LoadSceneMode.Single);
+        o.allowSceneActivation = false;
+        LeanTween.delayedCall(3, () =>
+        {
+            p1.Stop();
+            p2.Stop();
+            o.allowSceneActivation = true;
+        });
+    }
+}
---
+++ Assets/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public CanvasGroup fader;
+
+    private void Start()
+    {
+        fader.gameObject.SetActive(true);
+        LeanTween.alphaCanvas(fader, 0, 2);
+    }
+
+    public void StartGame()
+    {
+        AsyncOperation ao = SceneManager.LoadSceneAsync(1);
+        ao.allowSceneActivation = false;
+        
+        LeanTween.alphaCanvas(fader, 1, 2).setOnComplete(()=>{
+            ao.allowSceneActivation = true;            
+        });
+    }
+}
